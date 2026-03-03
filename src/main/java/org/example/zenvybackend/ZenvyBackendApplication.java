084c75a08809e49bff998d897d6c15f488ec774e
pranjal-kumar <pranjal.kumar@tothenew.com>
2026-03-03 10:13:14 +0530

    Initial commit - Spring Boot setup

---
+++ src/main/java/org/example/zenvybackend/ZenvyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.zenvybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenvyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZenvyBackendApplication.class, args);
+    }
+
+}
