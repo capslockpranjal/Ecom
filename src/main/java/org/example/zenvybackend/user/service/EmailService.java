6a025fa27485cac4a4ce3fb6bf1a0e094ba78fb9
pranjal-kumar <pranjal.kumar@tothenew.com>
2026-03-20 15:35:20 +0530

    asyn email

--- src/main/java/org/example/zenvybackend/user/service/EmailService.java
+++ src/main/java/org/example/zenvybackend/user/service/EmailService.java
@@ -1,6 +1,7 @@
 package org.example.zenvybackend.user.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -8,19 +9,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Async
+    @Async("mailExecutor")
     public void sendEmail(String to,String subject,String body){
+        try {
+            SimpleMailMessage message=new SimpleMailMessage();
 
-        SimpleMailMessage message=new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
 
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-
-        mailSender.send(message);
+            mailSender.send(message);
+        } catch (Exception ex) {
+            log.error("Failed to send email to {}", to, ex);
+        }
     }
 }
